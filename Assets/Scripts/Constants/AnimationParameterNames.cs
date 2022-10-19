c46dff53a60b3d6482ce08f0c80e763af933dcaf
Andrei Raketa <raketa420311@gmail.com>
2022-10-19 22:02:47 +0300

    [SFR-1] Character movement with ui joystick added

--- Assets/Scripts/Constants/AnimationParameterNames.cs
+++ Assets/Scripts/Constants/AnimationParameterNames.cs
@@ -1,8 +1,5 @@
 public static class AnimationParameterNames
 {
-    public const string Idle = "Idle";
-    public const string Aim = "Aim";
-    public const string Run = "Run";
-    public const string Dance = "Dance";
     public const string Faded = "Faded";
+    public const string Idle = "Idle";
 }
