1c5dcc82a7d485dd264e4901202e4b46e437aa5f
Andrei Raketa <raketa420311@gmail.com>
2022-10-26 21:43:56 +0300

    Revert "[SFR-6] Inventory with slots finished"
    This reverts commit d0c5bd257a3db469b060204a26c6cb3fdbb5d3aa.

---
+++ Assets/Scripts/Gameplay/Inventory/InventoryTester.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class InventoryTester : MonoBehaviour
+{
+    private IInventory _inventory;
+
+    private void Awake()
+    {
+        _inventory = new InventoryWithSlots(100);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            AddRandomItemJunk();
+
+        if (Input.GetKeyDown(KeyCode.O))
+            RemoveRandomItemJunk();
+    }
+
+    private void AddRandomItemJunk()
+    {
+        var rCount = Random.Range(1, 10);
+        
+        var itemJunk = new ItemJunk(10)
+        {
+            Amount = rCount
+        };
+
+        _inventory.TryToAdd(this, itemJunk);
+    }
+
+    private void RemoveRandomItemJunk()
+    {
+        var rCount = Random.Range(1, 10);
+        
+        _inventory.Remove(this, typeof(ItemJunk), rCount);
+    }
+}
